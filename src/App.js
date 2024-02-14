234d26f9e8be461e600e5d30a63a969bc866dad2
songblrd <radioanne6@gmail.com>
2024-02-13 23:21:28 -0500

    working in app.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      eeeee
-    </div>
+    <>
+      <Header>
+        <img src="logo.jpg" alt="little lemon logo"></img>
+      </Header>
+      <Nav>
+        <ul>
+          <li><a href="/" ></a>     </li>
+        </ul>
+      </Nav>
+      <Main></Main>
+      <Footer></Footer>
+    </>
   );
 }
 
